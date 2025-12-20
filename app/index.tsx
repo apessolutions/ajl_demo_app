28863188c763404b90ddf39ea90013fd5a2df4d5
Abdelrahman Mohamed Emam <54192914+AbdelrahmanMohamedEmam@users.noreply.github.com>
2025-12-21 00:09:34 +0200

    Integrate PostHog for analytics tracking; refactor main component to include HomeScreen with button click tracking. Update layout to enhance user interaction and remove placeholder text.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,42 @@
-import { Text, View } from "react-native";
+import { PostHogProvider, usePostHog } from 'posthog-react-native';
+import { useEffect } from 'react';
+import { Pressable, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <PostHogProvider apiKey="phc_lw7avLE9sYgTmocLLe0kpfsUobxSeFyAqDym62Mayah" options={{
+      host: "https://posthogv2.apessolutionsdev.com",
+
+    }}>
+
+      <HomeScreen />
+    </PostHogProvider>
   );
+}
+
+
+
+const HomeScreen = () => {
+  const posthog = usePostHog()
+
+  useEffect(() => {
+    // posthog.capture("Screen loaded", { screen: "Home" })
+    posthog.screen("Home");
+  }, [posthog]);
+  const handleClick = () => {
+    posthog.capture("Button clicked", { screen: "Home" });
+    console.log("Button clicked!");
+  };
+
+  return <View
+    style={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <Pressable onPress={handleClick}>
+      <Text>Edit app/index.tsx to edit this screen</Text>
+    </Pressable>
+  </View>;
 }
